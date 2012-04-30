2a27bd1968efb2ecdde3ca0e9e5daca101cf9401
Benjamin Doherty <bjd@pobox.com>
2012-04-30 11:37:47 -0500

    refactor javascript.

--- js/pageslide.region.js
+++ js/pageslide.region.js
@@ -4,43 +4,62 @@
 
   Drupal.behaviors.pageSlideRegion = {
     attach: function (context, settings) {
-      $('#pageslideregion', context).css({ position: 'fixed', top: 0, left: 0, bottom: 0, zIndex: 499, width: '260px', backgroundColor: '#ffffff' }).hide();
-      $('<button>Sidebar</button>', context).prependTo('body').button().click(function () {
-				var state = $('#pageslideregion').is(':hidden');
+      var $region = $('#pageslideregion', context),
+        $accordion = $('#pageslideregion > div'),
+        padding = $('body').css('paddingTop'),
+        options = {
+          navigation: true,
+          navigationFilter: function () {
+            return this.hash.toLowerCase() === ('#' + $.cookie('_pageSlideRegion'));
+          },
+          fillSpace: true,
+          header: '.block-title',
+          change: function (event, ui) {
+            if ($(ui.options.header, this).index(ui.newHeader) === $(ui.options.header, this).index(ui.oldHeader)) {
+              $.cookie('_pageSlideRegion', false);
+            } else {
+              $.cookie('_pageSlideRegion', ui.newHeader.attr('id'));
+            }
+          },
+          create: function (event, ui) {
+            $('body').css('marginLeft', $(this).width());
+          }
+        };
 
-        if (state) {
-          $('#pageslideregion').show(0, function () {
+      $region.css({
+        position: 'fixed',
+        top: padding,
+        left: 0,
+        bottom: 0,
+        zIndex: 499,
+        width: '260px',
+        backgroundColor: '#ffffff'
+      }).hide();
 
-            $('> div', this).accordion({
-              navigation: true,
-              navigationFilter: function () {
-                return this.hash.toLowerCase() === ('#' + $.cookie('_pageSlideRegion'));
-              },
-              fillSpace: true,
-              header: '.block-title',
-              change: function (event, ui) {
-                if ($(ui.options.header, this).index(ui.newHeader) === $(ui.options.header, this).index(ui.oldHeader)) {
-                  $.cookie('_pageSlideRegion', false);
-                } else {
-                  $.cookie('_pageSlideRegion', ui.newHeader.attr('id'));
-                }
-              },
-              create: function (event, ui) {
-                $('body').css('marginLeft', $(this).width());
+      $('<button>Sidebar</button>', context)
+        .prependTo('body')
+        .button()
+        .click(function () {
+          var state = $region.is(':hidden');
+
+          if (state) {
+            $region.show('slide', function () {
+              $accordion.accordion('resize');
+            });
+            $accordion.accordion(options);
+            $region.resizable({
+              handles: 'e',
+              resize: function (event, ui) {
+                $('body').css('marginLeft', ui.size.width);
               }
             });
-          }).resizable({
-            handles: 'e',
-            resize: function (event, ui) {
-              $('body').css('marginLeft', ui.size.width);
-            }
-          });
-        } else {
-          $('#pageslideregion > div').accordion('destroy');
-          $('#pageslideregion').resizable('destroy').hide();
-          $('body').css('marginLeft', 'auto');
-        }
-      });
+          } else {
+            $region.resizable('destroy').hide('slide', function() {
+              $accordion.accordion('destroy');
+            });
+            $('body').css('marginLeft', 'auto');
+          }
+        });
     }
   };
 
